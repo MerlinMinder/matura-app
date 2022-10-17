1afda1b6dfd81de6b37626806061586ca9d03c41
MerlinMinder <merlin.coding@gmail.com>
2022-10-17 12:46:06 +0200

    working neomorphism animation

--- components/home/Workouts.js
+++ components/home/Workouts.js
@@ -4,6 +4,8 @@ import { Neomorphism } from "../../Neomorphism";
 import styles from "../../Styles";
 import { neostyles } from "../../NeoStyles";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { useSharedValueEffect, useValue } from "@shopify/react-native-skia";
+import { useSharedValue, withDelay, withTiming } from "react-native-reanimated";
 
 export const Workouts = (props) => {
   let workouts = [
@@ -32,10 +34,27 @@ export const Workouts = (props) => {
       time: "128m 43s",
     },
   ];
+  const colorS1 = useValue("rgba(130, 130, 130, 0.7)");
+  const colorS2 = useValue("rgba(0, 0, 0, 0.5)");
+  const pressed = useSharedValue(false);
 
-  const gesture = Gesture.Tap().onBegin(() => {
-    console.log("Hello");
-  });
+  useSharedValueEffect(() => {
+    colorS1.current = pressed.value
+      ? "rgba(0, 0, 0, 0.5)"
+      : "rgba(130, 130, 130, 0.7)";
+
+    colorS2.current = pressed.value
+      ? "rgba(130, 130, 130, 0.7)"
+      : "rgba(0, 0, 0, 0.5)";
+  }, pressed);
+
+  const gesture = Gesture.Tap()
+    .onBegin(() => {
+      pressed.value = true;
+    })
+    .onFinalize(() => {
+      pressed.value = false;
+    });
 
   return (
     <View>
@@ -44,7 +63,14 @@ export const Workouts = (props) => {
       })}
       <GestureDetector gesture={gesture}>
         <View style={styles.workoutspluscontainer}>
-          <Neomorphism center settings={neostyles.workouts} inset>
+          <Neomorphism
+            center
+            settings={{
+              ...neostyles.workouts,
+              ...{ colorS1: colorS1, colorS2: colorS2 },
+            }}
+            inset
+          >
             <Image
               style={styles.workoutsplus}
               source={require("../../assets/pngs/Plus.png")}
